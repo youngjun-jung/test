bd97e184d150b249f3000f12afb76c08a46548cb
jyj <jminzzang@nate.com>
2025-01-18 16:43:40 +0900

    node add

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("111");
-console.log("111");
+var figlet = require("figlet");
+
+figlet("Hello Wor213123123ld!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});

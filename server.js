6beaa5d4457ae033ccf991bea5437369b3b97f17
jyj <jminzzang@nate.com>
2025-06-30 15:52:29 +0900

    sdafasdf

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://211.35.173.34:4002');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
